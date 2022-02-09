78297f0ecc955004ad6b0f17fe8ff4776f8fb309
Roman Zhukovski <roman.zhukovski@codex-soft.com>
2022-02-09 23:32:12 +0300

    feat: split folders, update docker

---
+++ hasura/migrations/default/1644438324428_run_sql_migration/down.sql
@@ -0,0 +1,6 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- INSERT INTO long_tails (tail,json_id) VALUES
+-- ('best-hello-ever', 1),
+-- ('best-hello-world-ever', 2),
+-- ('best-world-ever', 3);
---
+++ hasura/migrations/default/1644438324428_run_sql_migration/up.sql
@@ -0,0 +1,4 @@
+INSERT INTO long_tails (tail,json_id) VALUES
+('best-hello-ever', 1),
+('best-hello-world-ever', 2),
+('best-world-ever', 3);
